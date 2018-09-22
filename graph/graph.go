bee8400616c77fccd3fccf0884fdb6d9bcb4df6c
g0nta <mongolian.uxooi@gmail.com>
2018-09-22 20:06:50 +0900

    Change data structure and add some methods

--- graph/graph.go
+++ graph/graph.go
@@ -1,16 +1,12 @@
 package goraph
 
-//Vertex : struct of vertex that have id and weight
-type Vertex struct {
-	ID     int
-	Weight float32
-}
+//Vertex : 重みなしVertex. 重みありVertexは別の構造体で。。。？
+type Vertex uint
 
 //Edge : struct of edge. Maybe this struct don't need...
 type Edge struct {
-	From   Vertex
-	To     Vertex
-	Weight float32
+	From Vertex
+	To   Vertex
 }
 
 //Graph : グラフの構造体。privateにすべき？
@@ -19,5 +15,28 @@ type Edge struct {
 //隣接行列なら辺の問い合わせは定数。隣接行列持った方がいいかな。
 //隣接行列だけもつ。頂点の追加は隣接行列に行と列を足す。辺の追加は該当要素に1を立てる。とすれば良いのか？？？？
 //いやーMST求める時とかはEdge Set持ってた方が楽だったりするんだよな。。。
+// 頂点集合と辺集合と有向無向のフラグを持つようにする。
 type Graph struct {
+	VertexSet       map[Vertex]Vertex //Key: ID of vertex, Value: vertex.
+	EdgeSet         map[Edge]Edge
+	IsDirected      bool
+	LinkedList      map[Vertex][]Vertex
+	AdjacencyMatrix [][]Vertex
+}
+
+//New returns an empty graph.
+func New(isDirected bool) *Graph {
+	g := new(Graph)
+	g.VertexSet = make(map[Vertex]Vertex)
+	g.EdgeSet = make(map[Edge]Edge)
+	g.IsDirected = isDirected
+	g.LinkedList = make(map[Vertex][]Vertex)
+	g.AdjacencyMatrix = make([][]Vertex, 0)
+	return g
+}
+
+func (g *Graph) AddNode(v Vertex) error {
+	if _, isExist := g.VertexSet[v]; isExist==true {
+		return error.
+	}
 }
