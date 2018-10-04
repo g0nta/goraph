dfba8976cf1832fbc28af4545ab2ae16e0ca8670
g0nta <mongolian.uxooi@gmail.com>
2018-10-05 01:43:26 +0900

    Implement bfs. Untested.

--- algorithms/traversal/bfs.go
+++ algorithms/traversal/bfs.go
@@ -2,6 +2,7 @@ package traversal
 
 import (
 	"container/list"
+	"errors"
 
 	g "github.com/g0nta/goraph/graph"
 )
@@ -14,17 +15,43 @@ type BFS struct {
 }
 
 //NewBFS returns new BFS struct.
-func NewBFS(raph g.IGraph, visitFlagKey string, start interface{}) (*DFS, error) {
-	return nil, nil
+func NewBFS(graph g.IGraph, visitFlagKey string, start interface{}) (*BFS, error) {
+	bfs := new(BFS)
+	bfs.graph = graph
+	bfs.visitFlagKey = visitFlagKey
+	if graph.ContainsVertex(start) == false {
+		return nil, errors.New("graph does not have a start vertex")
+	}
+	bfs.queue = list.New()
+	bfs.queue.PushBack(start)
+	bfs.graph.UpdateVertexAttribute(start, visitFlagKey, true)
+	return bfs, nil
 }
 
 // Next updates a current vertex to a next vertex
 // and returns whether a next vertex exists or not.
 func (bfs *BFS) Next() bool {
-	return false
+	current := bfs.queue.Front().Value
+	visitFlagKey := bfs.visitFlagKey
+
+	neighbors := bfs.graph.GetNeighbors(current)
+	for key, value := range neighbors {
+		if flag, isExist := value[visitFlagKey]; isExist == false || flag == false {
+			bfs.queue.PushBack(key)
+		}
+	}
+
+	if current == bfs.queue.Front().Value {
+		bfs.queue.Remove(bfs.queue.Front())
+	}
+
+	if bfs.queue.Len() == 0 {
+		return false
+	}
+	return true
 }
 
 // Value returns current node.
 func (bfs *BFS) Value() interface{} {
-	return nil
+	return bfs.queue.Front().Value
 }
